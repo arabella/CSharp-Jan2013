c4fcf44410acc7e3d8b7583c5339892dafebf78f
Lina Ivanova <lina.greenaff@gmail.com>
2013-02-17 14:53:46 +0200

    added christmastree

---
+++ ChristmasTree/ChristmasTree/ChristmasTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class ChristmasTree
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 0  ; row < n-1; row++)
+            {
+                int dots = n - row - 2;
+                int asterisk = 2*row + 1;
+
+                Console.Write(new string('.', dots));
+                Console.Write(new string('*', asterisk));
+                Console.Write(new string('.', dots)); 
+
+                Console.WriteLine();
+                }
+            string bottom = new string('.', (n - 2)) + "*" + new string('.', (n - 2));
+            Console.WriteLine(bottom);
+                
+            }
+        }
+    
